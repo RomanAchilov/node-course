561a5428ce60bc241daf6f5cf36f0e0b6fe24280
RomanAchilov <dakaktak95@gmail.com>
2021-03-21 22:46:18 +0300

    task 2

---
+++ src/homework1/task1.2/index.ts
@@ -0,0 +1,26 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const path = 'src/homework1/task1.2/file'
+const csvFilePath = `${path}.csv`;
+const txtFilePath = `${path}.txt`;
+
+fs.writeFileSync(txtFilePath, '');
+
+interface Data {
+  Book: string;
+  Author: string;
+  Price: string;
+}
+
+csv({ ignoreColumns: /amount/gi })
+  .fromFile(csvFilePath)
+  .subscribe((data: Data) => {
+    fs.appendFileSync(txtFilePath, JSON.stringify(data) + '\n', 'utf8');
+  })
+  .on('done', () => {
+    console.log('DONE!');
+  })
+  .on('error', (err: any) => {
+    console.log(err);
+  });
